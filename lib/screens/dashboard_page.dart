d1a8b7b79e9c1070b807992f4a9ad17e3387ac96
Ashley Sheine <118508166+ashensn0w@users.noreply.github.com>
2024-01-25 00:38:49 +0800

    updated dashboard

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -155,12 +155,12 @@ class AddMoneyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primLightBg,
+      backgroundColor: primLightBg, // Use your desired background color
       appBar: AppBar(
         title: Text(
           'Add Money',
           style: TextStyle(
-            color: darkText,
+            color: darkText, // Use your desired text color
             fontFamily: 'Poppins',
             fontWeight: FontWeight.bold,
             fontSize: 26.0,
@@ -177,8 +177,116 @@ class AddMoneyPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text('This is the new screen.'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10), // Adjust the spacing as needed
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Amount',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF6B6A66),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Text(
+                        '₱', // Philippine Peso symbol
+                        style: TextStyle(
+                          fontSize: 28.0,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          color: darkText,
+                        ),
+                      ),
+                      SizedBox(width: 10), // Adjust spacing as needed
+                      Expanded(
+                        child: TextField(
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            color: darkText,
+                            // Add other style properties as needed
+                          ),
+                          decoration: InputDecoration(
+                            hintText: 'Enter amount',
+                            hintStyle: TextStyle(
+                              fontFamily: 'Poppins',
+                              // Add other hint style properties as needed
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFF2F2C2C)),
+                            ),
+                          ),
+                          cursorColor: Color(0xFF2F2C2C),
+                        ),
+                      ),
+                      SizedBox(width: 10), // Adjust spacing as needed
+                      Text(
+                        'PHP',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF6B6A66),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Title:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter title',
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Spending Limit:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter spending limit',
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Description:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                maxLines:
+                    null, // or specify the maximum number of lines you want
+                decoration: InputDecoration(
+                  hintText: 'Enter description',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
