166eb3dbb4bfb1bce80cbf678e844ef6bccb95b6
Mark Joseph Aquino <108459155+Zeldaxxx@users.noreply.github.com>
2024-01-24 19:11:51 +0800

    Add files via upload

---
+++ lib/utils/text.dart
@@ -0,0 +1,88 @@
+import 'dart:core';
+
+/* -- App Text Strings -- */
+
+// Global Text
+const String fullName = "Full Name";
+const String userName = "Username";
+const String userEmail = "Email";
+const String passWord = "Password";
+const String dateMonthly = "Monthly";
+const String dateWeekly = "Weekly";
+
+
+// -- Welcome Page
+
+const String tagLine = "We got it all for you";
+
+// -- Log In Page
+const String loginButton = "Login";
+const String signupPage = "Dont have any account? Sign up";
+
+// -- Sign Up Page
+const String confirmPassword = "Confirm Password";
+
+// Profile Page
+const String userProfile = "Profile";
+const String editProfile = "Edit Profile";
+const String personalInfo = "PERSONAL INFORMATION";
+const String pName = "Name";
+const String pBirthdate = "Birthdate";
+const String pSex = "Sex";
+const String contactInfo = "CONTACT INFO";
+const String changePicture = "Change Picture";
+const String pUpdate = "Update";
+const String pCancel = "Cancel";
+
+// Dashboard Page (Main Page)
+const String dashBoard = "Dashboard";
+const String physicalMoney = "Physical Money";
+const String userTransactions = "Transactions";
+const String viewAll = "View All";
+const String dToday = "Today";
+const String addExpenses = "Add Expenses";
+
+// Dashboard (Add Money, Add Expenses, View All)
+const String cashAmount = "Amount";
+const String description = "Description";
+const String title = "Title";
+const String attachFile = "Attach a File";
+const String save = "Save";
+const String addMoney = "Add Money";
+const String spendingLimit = "Spending Limit";
+const String dateMonths = "January";
+
+// Notification Page
+const String notifications = "Notifications";
+const String nUnread = "Unread";
+const String nAll = "All";
+
+// Summary Page
+const String sExpenses = "Expenses";
+const String forecastFutSpend = "FORECAST FUTURE SPENDING";
+const String budgetAlloc = "BUDGET ALLOCATION";
+const String futSpendPrediction = "Future Spending Prediction";
+const String monthlyExpenses = "Monthly Expenses";
+const String allocatedBudget = "BUDGET ALLOCATED FOR YOUR NEXT EXPENSE:";
+const String editAlloc = "EDIT ALLOCATION";
+const String totalBudget = "Total Budget Amount:";
+const String categories = "CATEGORIES";
+const String percentage = "PERCENTAGE";
+
+// Financial Content
+const String financialContent = "Financial Content";
+const String articles = "Articles";
+const String videos = "Videos";
+const String tips = "Tips";
+
+
+
+
+
+
+
+
+
+
+
+
