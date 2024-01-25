8b44ffc21f05a71b3445220caf170f80a2b83bd6
Mark Joseph Aquino <108459155+Zeldaxxx@users.noreply.github.com>
2024-01-26 02:31:38 +0800

    Add files via upload

---
+++ lib/utils/sizes.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class fontSizes{
+
+  // Font Sizes
+  static const double fontsizeSm = 18.0;
+  static const double fontsizeMd = 35.0;
+  static const double fontsizeLg = 45.0;
+
+  // Icon Sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 12.0;
+  static const double iconMd = 12.0;
+  static const double iconLg = 12.0;
+
+  // Padding and margin sizes
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  // Button sizes
+  static const double buttonHeight = 18.0;
+  static const double buttonRadius = 12.0;
+  static const double buttonWidth = 120.0;
+  static const double buttonElevation = 4.0;
+
+
+
+
+}
+
