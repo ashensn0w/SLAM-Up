161f50f8ba2abe807d1e5af389a210bf4e76dfbe
Ashley Sheine <118508166+ashensn0w@users.noreply.github.com>
2024-01-25 22:30:48 +0800

    show logo

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../utils/constants.dart';
+import 'dashboard_page.dart';
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Wait for 2 seconds and then navigate to the signin page
+    Timer(
+      Duration(seconds: 2),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => DashboardPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(slamupLogo),
+      ),
+    );
+  }
+}
