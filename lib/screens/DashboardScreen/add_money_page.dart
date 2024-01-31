3347d5f543462dda90be7d1959fc8cc67348c7b1
Ashley Sheine <118508166+ashensn0w@users.noreply.github.com>
2024-01-31 21:06:49 +0800

    update peso sign

--- lib/screens/DashboardScreen/add_money_page.dart
+++ lib/screens/DashboardScreen/add_money_page.dart
@@ -54,7 +54,7 @@ class AddMoneyPage extends StatelessWidget {
                         '₱', // Philippine Peso symbol
                         style: TextStyle(
                           fontSize: 28.0,
-                          fontFamily: 'Poppins',
+                          fontFamily: 'Roboto',
                           fontWeight: FontWeight.bold,
                           color: darkText,
                         ),
