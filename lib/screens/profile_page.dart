458192d01ad9f7f8c680b0503c5a09d1ac8d5c6f
Stefen Labinay <150441368+Kaiytee@users.noreply.github.com>
2024-02-01 14:02:06 +0800

    Update profile_page.dart

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -8,7 +6,6 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       /*appBar: AppBar(
         title: Text('Profile'),
         actions: [
@@ -18,25 +15,175 @@ class ProfilePage extends StatelessWidget {
           ),
         ],
       ),*/
-
       body: ListView(
         children: [
           SizedBox(
-            height: 60,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(0.0),
-            child: Column(
+            height: 350,
+            child: ColoredBox(
+              color: Color(0xFF43817F), // Change to your desired color
+
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('PROFILE',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      )),
-                ]),
+                  Padding(
+                    padding: EdgeInsets.only(top: 100, bottom: 20),
+                    child: Text('PROFILE',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.white,
+                        )),
+                  ),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                    //backgroundImage:
+                    //AssetImage('assets/images/clothing_icon.png'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 110),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EditProfile()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff153836),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text('Edit Profile',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
+          const SizedBox(
+            height: 50,
+          ),
+          SizedBox(
+            child: ColoredBox(
+              color: Color(0xffe8e4af), // Change to your desired color
+
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text('PERSONAL INFORMATION',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    )),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Name',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                const SizedBox(height: 4.0),
+                Text('example@email.com',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                const SizedBox(height: 4.0),
+                Text('1990-01-01',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                const SizedBox(height: 4.0),
+                Text('Male',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 110),
+            child: ElevatedButton(
+              onPressed: () {
+                /* Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EditProfile()),
+                );*/
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF43817F),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text('LOG OUT',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                    )),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EditProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*appBar: AppBar(
+        title: Text('Second Page'),
+      ),*/
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(children: [
+          SizedBox(
+            height: 60,
+          ),
+          Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Text('PROFILE',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                )),
+          ]),
           SizedBox(
             height: 10,
           ),
@@ -48,245 +195,131 @@ class ProfilePage extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EditProfile()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFF43817F),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(
-                5.0,
-              ),
-              child: Text('Edit Profile',
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Full Name',
                   style: TextStyle(
                     fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    //backgroundColor: Colors.grey,
                     fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                    color: Color.fromARGB(255, 255, 255, 255),
                   )),
-            ),
+            ],
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('PERSONAL INFORMATION',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                      backgroundColor: Colors.grey,
-                      fontSize: 18,
-                    )),
-                const SizedBox(height: 20.0),
-                Text('Name',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                    )),
-                const SizedBox(height: 4.0),
-                Text('example@email.com',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                    )),
-                const SizedBox(height: 4.0),
-                Text('1990-01-01',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                    )),
-                const SizedBox(height: 4.0),
-                Text('Male',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                    )),
-              ],
-            ),
-          ),
-
-
-          SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF43817F),
-              ),
-              child: Text(
-                'LOG OUT',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-              ))
-        ],
-      ),
-    );
-  }
-}
-
-// ignore: use_key_in_widget_constructors
-class EditProfile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      /*appBar: AppBar(
-        title: Text('Second Page'),
-      ),*/
-      body: ListView(children: [
-        SizedBox(
-          height: 10,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Text('PROFILE',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                )),
-          ]),
-        ),
-        SizedBox(
-          height: 0,
-        ),
-        CircleAvatar(
-          radius: 50,
-          //backgroundImage: AssetImage('lib/assets/images/img.jpg'),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => EditProfile()),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFF43817F),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(
-              5.0,
-            ),
-            child: Text('Change picture',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 12,
-                  fontWeight: FontWeight.normal,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: 50, // Desired width
-          height: 40, // Desired height
-          child: TextField(
+          TextField(
             decoration: InputDecoration(
-              labelText: 'Fullname', // Optional label
+              labelText: 'Full name', // Optional label
               border: OutlineInputBorder(), // Optional border
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: 50, // Desired width
-          height: 40, // Desired height
-          child: TextField(
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    //backgroundColor: Colors.grey,
+                    fontSize: 16,
+                  )),
+            ],
+          ),
+          TextField(
             decoration: InputDecoration(
               labelText: 'Email', // Optional label
               border: OutlineInputBorder(), // Optional border
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: 50, // Desired width
-          height: 40, // Desired height
-          child: TextField(
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Birthdate',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    //backgroundColor: Colors.grey,
+                    fontSize: 16,
+                  )),
+            ],
+          ),
+          TextField(
             decoration: InputDecoration(
               labelText: 'Birthdate', // Optional label
               border: OutlineInputBorder(), // Optional border
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: 50, // Desired width
-          height: 40, // Desired height
-          child: TextField(
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Password',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    //backgroundColor: Colors.grey,
+                    fontSize: 16,
+                  )),
+            ],
+          ),
+          TextField(
             decoration: InputDecoration(
               labelText: 'Password', // Optional label
               border: OutlineInputBorder(), // Optional border
             ),
           ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xff52dc57),
-            ),
-            child: Text(
-              'Update',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.normal,
-                fontSize: 16,
-                color: Color.fromARGB(255, 255, 255, 255),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ProfilePage()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                    ),
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 23,
+                      ),
+                    )),
               ),
-            )),
-        SizedBox(
-          height: 10,
-        ),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilePage()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.red,
-            ),
-            child: Text(
-              'Cancel',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.normal,
-                fontSize: 16,
-                color: Color.fromARGB(255, 255, 255, 255),
-              ),
-            ))
-      ]),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff52dc57),
+                  ),
+                  child: Text(
+                    'Update',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 23,
+                    ),
+                  )),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
