39f7fc7e5bcc8c2449083dd791dfdda3ae68c8f9
ashensn0w <juguetaashley@gmail.com>
2024-02-14 17:15:48 +0800

    stable sign up

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -4,7 +4,7 @@ import 'package:slam_up/utils/constants.dart';
 import '../utils/sizes.dart';
 
 class SignupPage extends StatefulWidget {
-  const SignupPage({Key? key}) : super(key: key);
+  const SignupPage({super.key});
 
   @override
   State<SignupPage> createState() => _SignupPageState();
